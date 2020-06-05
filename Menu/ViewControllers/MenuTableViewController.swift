46cef52b14e7fc79ffe04ea58bd7741bf8a048f3
kittinun chobthum <kittinun@corpjurist.com>
2020-06-06 00:07:16 +0700

    init project.

---
+++ Menu/ViewControllers/MenuTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuTableViewController.swift
+//  Menu
+//
+//  Created by Kittinun Chobtham on 6/6/2563 BE.
+//  Copyright © 2563 Kittinun Chobtham. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+
+}
